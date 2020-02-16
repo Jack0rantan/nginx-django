d2e620a8d47ce5f10e9471eac21daea967857411
k <kankan052@gmail.com>
2020-02-16 15:36:57 +0900

    [revise] url-rag properly for Django

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name ="develop" 
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('develop/coin', views.coin, name='coin'),
